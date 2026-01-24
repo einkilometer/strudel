a10d46ec60274e253c286b6e373dca31444ac18a
einkilometer <jukkaboehm@gmail.com>
2026-01-24 18:02:27 +0100

    Update bookmarklet to use GitHub raw URL

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,13 +1,13 @@
-javascript:(function(){
-  fetch("https://gist.githubusercontent.com/einkilometer/strudel/raw/datei.txt")
-    .then(r=>r.text())
-    .then(c=>window.location="https://strudel.cc/#"+c)
-})();
+javascript:(function(){ fetch("https://raw.githubusercontent.com/einkilometer/strudel/refs/heads/main/raw/datei.txt") .then(r=>r.text()) .then(c=>window.location="https://strudel.cc/#"+c) })();
 
 // ANLEITUNG:
-// 1. Ersetze GISTID mit der ID deines Gists
-//    Beispiel: https://gist.github.com/einkilometer/abc123def456 → GISTID = abc123def456
+
+// 1. kopiere die URL der RAW-Ansicht der datei.txt
+
+// Beispiel: https://raw.githubusercontent.com/einkilometer/strudel/refs/heads/main/raw/datei.txt
+
 // 2. Erstelle ein neues Bookmark in deinem Browser
+
 // 3. Benenne es (z.B. "Strudel Bookmarklet")
-// 4. Kopiere den obigen Code (mit deiner GISTID) als URL des Bookmarks
-// 5. Klicke auf das Bookmark, um deinen Strudel-Code zu laden
+
+// 4. Klicke auf das Bookmark, um deinen Strudel-Code zu laden
